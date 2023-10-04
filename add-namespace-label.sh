dd839760ace56e35b3cfb750e09c5b47775fb274
sj-williams <steve.williams@digital.justice.gov.uk>
2023-10-04 13:23:08 +0100

    add dev namespace condition

--- add-namespace-label.sh
+++ add-namespace-label.sh
@@ -5,7 +5,10 @@
 
 for dir in ./*; do
   if [ -d "$dir" ]; then
-    echo "Updating $dir/00-namespace.yaml"
-    yq -i '.metadata.labels."pod-security.kubernetes.io/audit" = "restricted"' "$dir/00-namespace.yaml"
+  # if dir name includes "dev" update the namespace yaml file
+    if [[ $dir == *"dev"* ]]; then
+      echo "Updating $dir/00-namespace.yaml"
+      yq -i '.metadata.labels."pod-security.kubernetes.io/audit" = "restricted"' "$dir/00-namespace.yaml"
+    fi
   fi
 done
